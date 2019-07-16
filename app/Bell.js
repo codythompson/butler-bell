41cadf557f92adef292f5ed1bbd5f043c34d1e29
Cody Thompson <cody.thompson22@gmail.com>
2019-07-15 23:30:14 -0700

    now bells will append a class based on a dote requests latest event type

--- app/Bell.js
+++ app/Bell.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import classNames from 'classnames'
+import maxBy from 'lodash/maxBy'
 
 import styles from './Bell.module.scss'
 import BellImage from '../assets/bell_a.jpg'
 // import BellImage from '../assets/bell_test.jpg'
 
-export default function ({name}) {
+export default function ({name, doteRequest}) {
+  let lastEventType = null;
+  if (doteRequest) {
+    lastEventType = maxBy(doteRequest.events, 'timestamp').type
+  }
+  console.log(lastEventType)
   return (
-    <div className={styles.Bell}>
+    <div className={classNames(styles.Bell, styles[lastEventType])}>
       <img className={styles.BellImg} src={BellImage} alt="servants bell"></img>
       <h2 className={styles.BellName}>{name}</h2>
     </div>
